ab8a65f7875af49a012656491edccf38584307b4
Huam Benvenutti <huambenvenutti@protonmail.com>
2021-11-18 14:00:57 -0300

    github button

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,7 @@
-import Image from 'next/image'
+import Image from 'next/image';
+
+import { SignInButton } from '../SignInButton';
+
 import styles from './styles.module.scss';
 
 export function Header() {
@@ -10,6 +13,7 @@ export function Header() {
           <a className={styles.active} href="">Home</a>
           <a href="">Posts</a>
         </nav>
+        <SignInButton />
       </div>
     </header>
   );
