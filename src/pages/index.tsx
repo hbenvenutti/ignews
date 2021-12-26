3e24853c42903233ee58f20e654f588ac580b8ab
Huam Benvenutti <huambenvenutti@protonmail.com>
2021-12-26 15:39:13 -0300

    subscribe buttton

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import { SubscribeButton } from '../components/SubscribeButton';
 
 
 import styles from './home.module.scss';
@@ -21,6 +22,7 @@ export default function Home() {
           <span>for $9.90 month</span>
           </p> 
 
+          <SubscribeButton />
         </section>
 
         <img src="/images/avatar.svg" alt="girl coding" />
