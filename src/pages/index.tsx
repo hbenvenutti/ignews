69e57d11862a923dc8f9539863e60e9a500851ac
Huam Benvenutti <huambenvenutti@protonmail.com>
2021-12-30 00:33:58 -0300

    get static props

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import Head from 'next/head'
 import { SubscribeButton } from '../components/SubscribeButton';
 import { stripe } from '../services/stripe';
@@ -28,7 +28,7 @@ export default function Home({ product }: HomeProps) {
           <h1>News about the <span>React</span> world.</h1>
 
           <p>Get access to all the publications  <br />
-          <span>for ${product.amount} month</span>
+          <span>for {product.amount} month</span>
           </p> 
 
           <SubscribeButton priceId={product.priceId}/>
@@ -41,7 +41,7 @@ export default function Home({ product }: HomeProps) {
 
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1KB3LTBB3HLOArNFZP5UQat3',
   {
     expand: ['product']
@@ -60,6 +60,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
      product, 
-    }
+    },
+    revalidate: 60 * 60 * 24 // 24 hours
   }
 }
